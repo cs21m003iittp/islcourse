b6fcf6175522203f5c291ee8b69cb0d7e49390f0
cs21m003iittp <90080186+cs21m003iittp@users.noreply.github.com>
2022-10-25 19:10:28 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,1 +1,33 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from sklearn.metrics import classification_report, confusion_matrix, precision_score
 
+from torchvision import datasets
+from torchvision.transforms import ToTensor, ToPILImage
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f"Using {device} device")
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
