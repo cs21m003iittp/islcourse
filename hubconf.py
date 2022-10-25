d913f1359a303c97eb4c9617855c4ab52370817e
cs21m003iittp <90080186+cs21m003iittp@users.noreply.github.com>
2022-10-25 20:00:22 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -101,17 +101,3 @@ def _test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     
-def train(train_dataloader, test_dataloader, epochs=5):
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer)
-        test(test_dataloader, model, loss_fn)
-    print("Done!")
-
-def sample_test(model, test_data):
-    model.eval()
-    x, y = test_data[0][0], test_data[0][1]
-    with torch.no_grad():
-        pred = model(x)
-        predicted, actual = classes[pred[0].argmax(0)], classes[y]
-        print(f'Predicted: "{predicted}", Actual: "{actual}"')
