87a1337b96e4fc6f4d1b5dd91b820072823482c6
cs21m003iittp <90080186+cs21m003iittp@users.noreply.github.com>
2022-11-21 09:55:56 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,2 +1,31 @@
-
 import torch
+import numpy as np
+import sklearn
+from sklearn.datasets import make_blobs,make_circles
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+
+
+def get_data_blobs(n_points=100):
+  X, y = make_blobs(n_samples=n_points)
+  return X,y
+
+def get_data_circles(n_points=100):
+  pass
+  X, y = make_circles(n_samples=n_points)
+  return X,y
+
+
+def build_kmeans(X=None,k=10):
+  
+  km = KMeans(n_clusters=k)
+  km.fit(X)
+  return km
+
+
+def assign_kmeans(km=None,X=None):
+  ypred = km.predict(X)
+  return ypred
